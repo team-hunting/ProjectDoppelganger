51403a14511dd03d46abf9017bedf884bf091ba4
Travis Hunting <travis.hunting@gmail.com>
2022-01-17 09:17:31 +1300

    Updating index route

--- app/main.py
+++ app/main.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/') # Equivalent to: app.add_url_rule('/', '', index)
 def index():
-    return "<h1>Welcome Friend</h1>"
+    return render_template('comic.html')
 
 @app.route('/comic')
 def test():
@@ -13,7 +13,6 @@ def test():
 @app.route('/api/comic/comicinfo', methods=['POST'])
 def comicInfo():
     content_type = request.headers.get('Content-Type')
-    print(content_type)
 
     if content_type == 'application/json':
         content = request.get_json()
