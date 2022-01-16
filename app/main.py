7fdc5129e07f417d7a51e58291b3eb682cc229b9
Travis Hunting <travis.hunting@gmail.com>
2022-01-16 15:49:54 +1300

    Adding JS to make a request and then display data from Flask

--- app/main.py
+++ app/main.py
@@ -1,11 +1,23 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/') # Equivalent to: app.add_url_rule('/', '', index)
 def index():
     return "<h1>Welcome Friend</h1>"
 
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+@app.route('/api/comic/comicinfo', methods=['POST'])
+def comicInfo():
+    content_type = request.headers.get('Content-Type')
+    print(content_type)
+
+    if content_type == 'application/json':
+        content = request.get_json()
+        print(content)
+        print(content['url'])
+
+    return {"test": "test"}
