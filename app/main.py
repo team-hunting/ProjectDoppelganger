ebcdf58a5f84039d4833874c736d3c148cfa15fc
Travis Hunting <travis.hunting@gmail.com>
2022-01-19 18:24:02 +1300

    Building out scraping functionality

--- app/main.py
+++ app/main.py
@@ -4,6 +4,14 @@ from bs4 import BeautifulSoup as bs
 
 app = Flask(__name__)
 
+headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Max-Age': '3600',
+        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
+    }
+
 @app.route('/') # Equivalent to: app.add_url_rule('/', '', index)
 def index():
     return render_template('comic.html')
@@ -33,6 +41,18 @@ def comicInfo():
 def issueInfo():
     content_type = request.headers.get('Content-Type')
 
+    # For testing purposes
+    return {"title": "Sandman...Lucifer...Whatever", "issues":[[
+    "Issue-1",
+    "https://readcomiconline.li/Comic/Sandman-Presents-Lucifer/Issue-1?id=37194"
+],[
+    "Issue-2",
+    "https://readcomiconline.li/Comic/Sandman-Presents-Lucifer/Issue-2?id=37196"
+],[
+    "Issue-3",
+    "https://readcomiconline.li/Comic/Sandman-Presents-Lucifer/Issue-3?id=37198"
+] ]}
+
     if content_type == 'application/json':
         content = request.get_json()
         print("CONTENT: " + str(content))
@@ -43,11 +63,30 @@ def issueInfo():
         issues = [(getIssueName(issueLink, "/Comic/" + title), "https://readcomiconline.li" + issueLink) for issueLink in issueLinks]
         print(issues)
         
-        #TODO: process the issue tuples using JS in comic.html
         return {"title": title, "issues": issues}
 
     else:
         return {}
+
+@app.route('/api/comic/scrapeissue', methods=['POST'])
+def scrapeIssue():
+    content_type = request.headers.get('Content-Type')
+
+    if content_type == 'application/json':
+        content = request.get_json()
+        print("CONTENT: " + str(content))
+        url = content['issueLink']
+        print("URL: " + url)
+        if "&readType=1" not in url:
+            print("Adding '&readType=1' to url")
+            url = url + "&readType=1"
+        hq = content['hq']
+        print("HQ: " + str(hq))
+        print("TYPEOF HQ: ", type(hq))
+        #return scrapeImageLinksFromIssue(url, hq)
+    
+    # this only hits if the wrong type of request is sent
+    return {"imageLinks":["https://upload.wikimedia.org/wikipedia/commons/thumb/7/7a/Klaus_Barbie.jpg/176px-Klaus_Barbie.jpg","https://upload.wikimedia.org/wikipedia/commons/thumb/7/79/Hoodoo_Mountain.jpg/243px-Hoodoo_Mountain.jpg"]}
 
 def getComicTitle(url, issue=False):
     prefix = "https://readcomiconline.li"
@@ -67,14 +106,6 @@ def getComicTitle(url, issue=False):
     return title
 
 def getLinksFromStartPage(url):
-    headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Methods': 'GET',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Max-Age': '3600',
-        'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:52.0) Gecko/20100101 Firefox/52.0'
-    }
-
     req = requests.get(url, headers)
     soup = bs(req.content, 'html.parser')
 
@@ -99,3 +130,35 @@ def getIssueName(issueLink, startURL):
     if issueName[0] == "/":
         issueName = issueName[1:]
     return issueName
+
+def scrapeImageLinksFromIssue(url, lowres=True):
+    req = requests.get(url, headers)
+    soup = bs(req.content, 'html.parser')
+    soup = soup.prettify()
+    lines = soup.split("\n")
+    imageLinks = []
+
+    print(lines)
+
+    for line in lines:
+        if "https://2.bp.blogspot.com" in line:
+            imageUrl = extractImageUrlFromText(line, lowres)
+            imageLinks.append(imageUrl)
+
+        # if checkForCaptcha(line):
+        #     solveCaptcha(url)
+        #     return scrapeImageLinksFromIssue(url, lowres)
+
+    return {"imageLinks":imageLinks}
+
+def extractImageUrlFromText(text, hq):
+    # urlEnd = text.find("s1600")
+    urlEnd = text.find(")")
+    urlStart = text.find("https")
+    output = text[urlStart:urlEnd-1]
+    print("extracted image link: " + output)
+    # verbose
+    # print("extractImageUrlFromText output ", output)
+    if hq:
+        output = output.replace("s1600","s0")
+    return output
